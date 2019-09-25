d619fbc84272572491a594287257f01629bf1033
Gerard Hynes <gkhynes@gmail.com>
2019-09-25 22:09:36 +0100

    Set up launches query

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import Launches from "./components/Launches";
+
+import "./App.css";
+import logo from "./logo.png";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <div style={{ backgroundColor: "#000000" }}>
+          <img
+            src={logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+        </div>
+        <div className="container">
+          <Launches />
+        </div>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
