cad8e7fb06dcde6391ed174b225b92d26712c509
Gerard Hynes <gkhynes@gmail.com>
2019-09-28 11:47:01 +0100

    Prepare for Heroku deploy

---
+++ public/precache-manifest.25d3e3685cdbab813ae6503322e33514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a0149c21d193d9bc908c019a269697",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d969e3f9b0aa415c8439",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c90b26d638efc7a013f4",
+    "url": "/static/js/2.051690ba.chunk.js"
+  },
+  {
+    "revision": "d969e3f9b0aa415c8439",
+    "url": "/static/js/main.2a2053c2.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
